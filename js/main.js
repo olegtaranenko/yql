2db78b33111c93f6f1ccfbf54dc1f673b1bcec29
Oleg Taranenko <olegtaranenko@gmail.com>
2017-10-20 16:37:43 +0200

    borrow solution from ikromin

--- js/main.js
+++ js/main.js
@@ -1,27 +1,68 @@
 /**
- * The method is called after the
+ * The method is called after the user selects one or other location to view its forecast
  * @param place
  */
 function placeSelected(place) {
-    $.ajax({
-        url:'https://query.yahooapis.com/v1/public/yql',
-        data:{
-            q: 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+place+"\") and u='C'",
-            format: 'json'
-        },
-        success: function (data) {
-            var count=data.query.count;
-            var forecastItem = data.query.results.channel.item;
-            console.log (forecastItem);
-            var description = forecastItem.description;
-            console.log (description);
+    // redirect to locally stored json
+    var debug = getUrlParam("debug");
+    var iconUrl = 'https://s.yimg.com/zz/combo?a/i/us/we/52/';
 
-            $('#description').html(description);
+    if (debug == 1) {
+        var placeSuffix;
+        switch (place) {
+            case 'Cologne, Germany':
+                placeSuffix='k';
+                break;
+            default:
+                placeSuffix='d';
         }
-    });
+
+        $.getJSON("api/yql-" + placeSuffix + ".json", yqlSuccess);
+    } else {
+
+        $.ajax({
+            url:'https://query.yahooapis.com/v1/public/yql',
+            data:{
+                q: 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+place+"\") and u='C' limit 3 ",
+                format: 'json'
+            },
+            success: yqlSuccess
+        });
+    }
+
+    // will be used as callback, either by loading from the yahoo or from local stored json.
+    function yqlSuccess(data) {
+        if (data.query.count > 0) {
+            var channel = data.query.results.channel;
+            var units = channel.units;
+            var u = units.temperature;
+            $('#description').empty();
+
+            jQuery.each(channel.item.forecast, function(idx, f) {
+                console.log(f);
+                var c = $('#weatherTemplate').clone();
+                c.find('.weather_date').text(f.date);
+                c.find('.weather_temp_min').text(f.low + u);
+                c.find('.weather_temp_max').text(f.high + u);
+                c.find('.weather_icon').attr('src', iconUrl + f.code + '.gif');
+                c.find('.weather_text').text(f.text);
+
+                c.css('display', 'inline-block');
+
+
+                c.appendTo('#description');
+            });
+        }
+    }
 }
 
-
+// initia event listener for the select control, using jquery facility
 $('#standort').on('change', function () {
-    placeSelected(this.value);
+    var place=this.value;
+    placeSelected(place);
 });
+
+function getUrlParam(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results && results[1] || 0;
+}
